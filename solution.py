9a6fec207ae42e80a5812581c9bb9d5eac362bf5
PoMaHCep <80079887+PoMaHCep@users.noreply.github.com>
2023-04-06 19:13:46 +1000

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+import scipy.stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1311912267 # Ваш chat ID, не меняйте название переменной
+
+#ускорение a = 2*S/t^2
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    res = st.t.interval(alpha=(1 - p), df=len(x)-1, loc=x.mean(),scale=st.sem(x))
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    #loc = x.mean()
+    #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+    #return loc - scale * norm.ppf(1 - alpha / 2), \
+    #       loc - scale * norm.ppf(alpha / 2)
+    return 2*res[0]/4225, \
+            2*res[1]/4225
